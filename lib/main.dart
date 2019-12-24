545fbad9a34a7fabedfe2c91ce2da0a545681d50
Paulo Cesar Ferreira de Mello <pcfmello@MacBook-Air-de-Paulo.local>
2019-12-24 00:25:43 -0300

    Criar funcionalidades de adição e subtração

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,89 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
       title: 'Contador de Pessoas',
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Home()));
+}
+  class Home extends StatefulWidget {
+    @override
+    _HomeState createState() => _HomeState();
+  }
+
+  class _HomeState extends State<Home> {
+
+  int _number_of_people = 0;
+
+  void _increasePeople() {
+    setState(() {
+      _number_of_people += 1;
+    });
+  }
+
+  void _decreasePeople() {
+    setState(() {
+      _number_of_people -= 1;
+    });
+  }
+
+  String _showMessage() {
+    if (_number_of_people < 0) return "Mundo Invertido?!";
+    if (_number_of_people <= 10) return "Pode Entrar :)";
+    return "Lotado!";
+  }
+
+    @override
+    Widget build(BuildContext context) {
+      return Stack(
         children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          Image.asset(
+            "images/restaurant.jpg",
+            fit: BoxFit.cover,
+            height: 1000,
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: FlatButton(
-                  child: Text(
-                    "+1",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
-                  ),
-                  onPressed: () {},
-                ),
+              Text(
+                "Pessoas: $_number_of_people",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: FlatButton(
-                  child: Text(
-                    "-1",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                      onPressed: () {
+                        debugPrint("+1");
+                        _increasePeople();
+                      },
+                    ),
                   ),
-                  onPressed: () {},
-                ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                      onPressed: () {
+                        _decreasePeople();
+                      },
+                    ),
+                  ),
+                ],
               ),
+              Text(_showMessage(),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 30.0))
             ],
-          ),
-          Text("Pode entrar",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0))
+          )
         ],
-      )));
-}
+      );
+    }
+  }
